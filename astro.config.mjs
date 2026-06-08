d2f202c21f905d304d3e5c8fefce4b6d8a9d9245
JP Sauve <jean-paul@grokology.ca>
2026-06-08 15:11:22 -0400

    adds rss and finishes tut

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://1029384756-astroblog.netlify.app/',
+  integrations: [preact()],
+});
